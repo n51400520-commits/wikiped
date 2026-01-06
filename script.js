36239a4399cb28f3b5e453c5f02e441a629cede7
Shionss <n51400520@gmail.com>
2026-01-06 17:23:21 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,17 @@
+// Берём элементы
+const searchInput = document.querySelector("header input");
+const cards = document.querySelectorAll(".card");
+
+// Слушаем ввод
+searchInput.addEventListener("input", function() {
+  const query = this.value.toLowerCase();
+
+  cards.forEach(card => {
+    const text = card.innerText.toLowerCase();
+    if (text.includes(query)) {
+      card.style.display = "block";
+    } else {
+      card.style.display = "none";
+    }
+  });
+});
